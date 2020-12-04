fe5a49eaaa612f6a6c48dc3db3c3049687c80dc9
Jeroen Heijmans <github@jeroenheijmans.nl>
2020-12-04 22:06:52 +0100

    Day 4 part 2

--- src/day04.ts
+++ src/day04.ts
@@ -1145,19 +1145,19 @@ pid:745302991 byr:1969 hcl:#7394c7
 `;
 
 const example = `
-ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-byr:1937 iyr:2017 cid:147 hgt:183cm
+eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
 
-iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
-hcl:#cfa07d byr:1929
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
 
-hcl:#ae17e1 iyr:2013
-eyr:2024
-ecl:brn pid:760753108 byr:1931
-hgt:179cm
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
 
-hcl:#cfa07d eyr:2025 pid:166559648
-iyr:2011 ecl:brn hgt:59in
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007
 `;
 
 const required = [
@@ -1177,6 +1177,7 @@ let data = input
   .filter(x => !!x)
   .map(x => x
     .split(/\s+/)
+    .sort()
     .reduce((obj: Dictionary<string>, field) => {
       obj[field.split(":")[0]] = field.split(":")[1];
       return obj;
@@ -1211,12 +1212,13 @@ data.forEach(pp => {
   const hgt = parseInt(pp["hgt"].split(/cm|in/)[0]);
   if (pp["hgt"].endsWith("in") && (hgt < 59 || hgt > 76)) {
     return;
-  }
-  if (pp["hgt"].endsWith("cm") && (hgt < 150 || hgt > 193)) {
+  } else if (pp["hgt"].endsWith("cm") && (hgt < 150 || hgt > 193)) {
+    return;
+  } else if(!pp["hgt"].endsWith("cm") && !pp["hgt"].endsWith("in")) {
     return;
   }
 
-  if (!pp["hcl"].match(/#[\da-f]{6}/)) {
+  if (!pp["hcl"].match(/^#[\da-f]{6}$/)) {
     return;
   }
 
@@ -1224,7 +1226,7 @@ data.forEach(pp => {
     return;
   }
 
-  if (!pp["pid"].match(/\d{9}/)) {
+  if (!pp["pid"].match(/^\d{9}$/)) {
     return;
   }
 
