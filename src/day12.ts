b187adca2f798d70e2dd0dd441759b94222db408
Jeroen Heijmans <github@jeroenheijmans.nl>
2020-12-12 14:26:03 +0100

    Prepare day 12

---
+++ src/day12.ts
@@ -0,0 +1,22 @@
+(() => {
+
+const input = `
+`;
+
+const example = `
+`;
+
+let data = input
+  .trim()
+  .split(/\r?\n/)
+  .filter(x => !!x)
+  .map(x => x);
+
+let part1 = 0, part2 = 0;
+
+
+
+console.log('Part 1:', part1);
+console.log('Part 2:', part2);
+
+})();
